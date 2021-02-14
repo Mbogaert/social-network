a5a9e0f8b3fb396a2368d01768266d4dbde45e04
Mathew B <bogaert@email.arizona.edu>
2021-02-13 21:03:30 -0700

    initial route and controllers files

---
+++ controllers/thought-controller.js
@@ -0,0 +1,13 @@
+const { User, Thought } = require('../models');
+
+const thoughtController = {
+    // get all thoughts
+    // get one thought by id
+    // create thought
+    // update thought by id
+    // delete thought
+    // add Reaction to thought
+    // remove reaction
+}
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,11 @@
+const { User } = require('../models');
+
+const userController = {
+    // get all Users
+    // get one user by id
+    // create User
+    // update User by id
+    // delete user
+}
+
+module.exports = userController;
