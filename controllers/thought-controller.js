7b9c3726b991b7d5fcae517969e1528184f5ac24
Mathew B <bogaert@email.arizona.edu>
2021-02-13 23:33:21 -0700

    create though is working though the catch for no user found with id isn't working

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -2,8 +2,41 @@ const { User, Thought } = require('../models');
 
 const thoughtController = {
     // get all thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
     // get one thought by id
     // create thought
+    createThought({ body }, res) {
+        // console.log(body);
+        Thought.create(body)
+          .then((dbThoughtData) => {
+            return User.findOneAndUpdate(
+              { _id: body.userId },
+              { $push: { thoughts: dbThoughtData._id } },
+              { new: true }
+            )
+            .populate(
+                { path: "thoughts",
+            select: "-__v",
+        })
+        
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No user found with this id!" });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => res.json(err));
+        })
+        .catch((err) => res.status(400).json(err));
+      },
     // update thought by id
     // delete thought
     // add Reaction to thought
