acdbdf1e260320e60bb3fa7216a44df17ffa3752
Mathew B <bogaert@email.arizona.edu>
2021-02-15 01:10:41 -0700

    fixed some small bugs

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -17,11 +17,18 @@ const thoughtController = {
             _id: params.id,
         })
         .select("-__v")
-        .then((dbThoughtData) => res.json(dbThoughtData))
-        .catch((err) => {
+        .then((dbThoughtData) => {
+            if (!dbThoughtData) {
+              return res.status(404).json({
+                message: "no thought with that id!",
+              });
+            }
+            res.status(200).json(dbThoughtData);
+          })
+          .catch((err) => {
             console.log(err);
             res.sendStatus(400);
-        })
+          });
     },
     // create thought
     createThought({ body }, res) {
@@ -41,11 +48,14 @@ const thoughtController = {
               res.status(404).json({ message: "No user found with this id!" });
               return;
             }
-            res.json(dbUserData);
+            res.status(200).res.json(dbUserData);
           })
           .catch((err) => res.json(err));
         })
-        .catch((err) => res.status(400).json(err));
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(400);
+          });
       },
     // update thought by id
     updateThought({ params, body }, res) {
@@ -67,19 +77,22 @@ const thoughtController = {
                 return res.status(404).json({ message: 'No thought with this id!'})
             }
             return User.findOneAndUpdate(
-                { _id: body.id },
-                { $pull: { thoughts: deletedThought._id } },
+                { _id: params.userId },
+                { $pull: { thoughts: params.id } },
                 { new: true }
             );
         })
         .then(dbUserData => {
             if (!dbUserData) {
-                res.status(404).json({ message: 'No user found with this id!'})
+                res.status(200).json({ message: 'Deleted thought'})
                 return;
             }
             res.json(dbUserData);
         })
-        .catch(err => res.json(err));
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
     },
     // add Reaction to thought
     addReaction({ params, body }, res) {
